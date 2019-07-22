64e3575f3c40acb96d2842b141b2b578eddf75f9
kod3180 <kod3180@gmail.com>
2019-07-22 21:14:09 +0000

    image upload to cloudinary added

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
+const formidable = require('formidable');
 
 exports.signup = async function(req, res, next) {
   try {
@@ -67,3 +68,65 @@ exports.signin = async function(req, res, next) {
   }
 }
 
+
+exports.uploadImage = (req, res, next) => {
+  const path = require('path');
+  const fs = require("fs");
+  let form = new formidable.IncomingForm();
+  form.keepFilenames = true;
+  form.uploadDir = path.join(__dirname, "../") + "/uploads";
+  form.keepExtensions = true;
+  form.maxFileSize = 5 * 1024 * 1024;
+  form.multiples = false;
+
+  form.parse(req, function(err, fields, file) {
+    let filePath = file[""].path;
+      if(err) {
+        res.json({
+          message: `Cannot upload image. Error is ${err}`
+        })
+      }
+
+      if(file[''].type !== "image/png" && file[''].type !== "image/jpeg") {
+        fs.unlinkSync(filePath);
+        return res.status(400).json({error: "invalid file type"});
+      }
+
+      const cloudinary = require("cloudinary").v2;
+      cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET
+      });
+      
+       cloudinary.uploader.upload(
+        filePath,
+        {
+          tags: "profileImage",
+          resource_type: "image"
+        }, 
+        function(err, image) {
+          try {
+            if (err) return res.send(err);
+            fs.unlinkSync(filePath);
+            db.User.findById(req.params.id, (err, user) => {
+              if(err) {
+                res.json({
+                  status: 400,
+                  message: "You cannot perform this action"
+                })
+              }
+              user.profileImage = image.url;
+              user.save();
+              return res.status(200).json({profileImage:user.profileImage});
+            });  
+          } catch (error) {
+            return next({
+              status: 400,
+              message: "An error occured"
+            });
+          }    
+       }
+     );
+  });
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin } = require('../handlers/auth');
+const { signup, signin, uploadImage } = require('../handlers/auth');
 
 router.post('/signup', signup);
 router.post('/signin', signin);
+router.put('/user/:id/image', uploadImage);
 
 module.exports = router;
