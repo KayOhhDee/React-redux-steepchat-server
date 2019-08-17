c76448316966be1a8348871b4daaa6e3bcf8e3c5
kod3180 <kod3180@gmail.com>
2019-08-17 09:08:22 +0000

    changes made for deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/user');
 const { loginRequired, isCorrectUser } = require('./middleware/auth');
 const db = require("./models");
 
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(bodyParser.json());
